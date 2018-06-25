bd754f1bebf45466ab9b4fe822ae69dd15dfa780
Adhi Satria <adhisatria1@outlook.com>
2018-06-25 11:07:48 +0700

    Install Socialite, add controller, model, and migration

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Laravel\Socialite;
 
 class LoginController extends Controller
 {
@@ -25,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/dashboard';
 
     /**
      * Create a new controller instance.
@@ -35,5 +36,36 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectToProvider()
+    {
+        return Socialite::driver('twitter')->redirect();
+    }
+
+    public function handleProviderCallback()
+    {
+        $check_user = Socialite::driver('twitter')->user();
+        $access_token = $check_user->token;
+        $access_token_secret = $check_user->tokenSecret;
+        $twitter_id = $check_user->getId();
+        $find_user = User::where('twitter_id', '=', $twitter_id)->first();
+        if ($find_user) {
+            $user = $find_user;
+        } else {
+            $user = User::create([
+                'name' => $check_user->getName(),
+                'email' => $check_user->getEmail(),
+                'twitter_id' => $twitter_id,
+                'remember_token' => $access_token
+            ]);
+            $user->save();
+        }
+        setcookie("access_token", " ", time()-3600);
+        setcookie("access_token", $access_token, time()+60*10);
+        setcookie("access_token_secret", " ", time()-3600);
+        setcookie("access_token_secret", $access_token_secret, time()+60*10);
+        auth()->login($user);
+        return redirect()->to('/dashboard');
     }
 }
